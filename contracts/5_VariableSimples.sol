2af4a58f00dec7f8a75602287ce60f3801c63e0f
luisjoseflores <ljfloresr@gmail.com>
2026-09-19 19:15:47 -0500

    agregar constructor

--- contracts/5_VariableSimples.sol
+++ contracts/5_VariableSimples.sol
@@ -5,6 +5,10 @@ pragma solidity >=0.8.2 <0.9.0;
 contract VariableSimples {
     uint256 private edad; //por defecto toma el valor de cero
 
+    constructor(uint256 _edad) {
+        edad = _edad;
+    }
+
     function cambiarEdad(uint256 _edad) public {
         edad = _edad;
     }
